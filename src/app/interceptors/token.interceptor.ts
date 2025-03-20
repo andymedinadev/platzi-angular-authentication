25452570f8898ca563767b9e445c402c70346081
andymedinadev <107598069+andymedinadev@users.noreply.github.com>
2025-03-20 19:44:17 -0300

    feat: enhance token interceptor to support automatic token refresh

--- src/app/interceptors/token.interceptor.ts
+++ src/app/interceptors/token.interceptor.ts
@@ -1,10 +1,16 @@
-import { inject } from '@angular/core';
 import {
-  HttpContext,
-  HttpContextToken,
+  HttpRequest,
+  HttpHandlerFn,
+  HttpEvent,
   HttpInterceptorFn,
+  HttpContextToken,
+  HttpContext,
 } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { Observable, switchMap } from 'rxjs';
+
 import { TokenService } from '@services/token.service';
+import { AuthService } from '@services/auth.service';
 
 const CHECK_TOKEN = new HttpContextToken<boolean>(() => false);
 
@@ -12,16 +18,47 @@ export function checkToken() {
   return new HttpContext().set(CHECK_TOKEN, true);
 }
 
-export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
+export const tokenInterceptor: HttpInterceptorFn = (
+  request: HttpRequest<unknown>,
+  next: HttpHandlerFn,
+): Observable<HttpEvent<unknown>> => {
   const tokenService = inject(TokenService);
-  const accessToken = tokenService.getToken();
+  const authService = inject(AuthService);
 
-  if (accessToken) {
-    const authRequest = req.clone({
-      headers: req.headers.set('Authorization', `Bearer ${accessToken}`),
-    });
+  const addToken = (
+    req: HttpRequest<unknown>,
+  ): Observable<HttpEvent<unknown>> => {
+    const accessToken = tokenService.getToken();
+    if (accessToken) {
+      const authRequest = req.clone({
+        headers: req.headers.set('Authorization', `Bearer ${accessToken}`),
+      });
+      return next(authRequest);
+    }
+    return next(req);
+  };
 
-    return next(authRequest);
+  const updateAccessTokenAndRefreshToken = (
+    req: HttpRequest<unknown>,
+  ): Observable<HttpEvent<unknown>> => {
+    const refreshToken = tokenService.getRefreshToken();
+    const isValidRefreshToken = tokenService.isValidRefreshToken();
+    if (refreshToken && isValidRefreshToken) {
+      return authService
+        .refreshToken(refreshToken)
+        .pipe(switchMap(() => addToken(req)));
+    }
+    return next(req);
+  };
+
+  if (request.context.get(CHECK_TOKEN)) {
+    const isValidToken = tokenService.isValidToken();
+    if (isValidToken) {
+      return addToken(request);
+    } else {
+      return updateAccessTokenAndRefreshToken(request);
+    }
   }
-  return next(req);
+
+  return next(request);
 };
