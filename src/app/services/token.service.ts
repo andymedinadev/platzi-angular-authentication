240e9ed9e6efd46e0d45f1ab2f60e2d2bb71160e
andymedinadev <107598069+andymedinadev@users.noreply.github.com>
2025-03-20 19:12:26 -0300

    feat: implement refresh token handling and validation

--- src/app/services/token.service.ts
+++ src/app/services/token.service.ts
@@ -11,6 +11,7 @@ export class TokenService {
   saveToken(token: string): void {
     setCookie('token-trello', token, { expires: 365, path: '/' });
   }
+
   getToken(): string | undefined {
     const token = getCookie('token-trello');
     return token;
@@ -22,6 +23,31 @@ export class TokenService {
 
   isValidToken(): boolean {
     const token = this.getToken();
+    if (!token) return false;
+
+    const decodeToken = jwtDecode<JwtPayload>(token);
+    const exp = decodeToken?.exp;
+
+    if (!exp) return false;
+
+    return exp * 1000 > Date.now();
+  }
+
+  saveRefreshToken(token: string): void {
+    setCookie('refresh-token-trello', token, { expires: 365, path: '/' });
+  }
+
+  getRefreshToken(): string | undefined {
+    const token = getCookie('refresh-token-trello');
+    return token;
+  }
+
+  removeRefreshToken(): void {
+    removeCookie('refresh-token-trello');
+  }
+
+  isValidRefreshToken(): boolean {
+    const token = this.getRefreshToken();
     if (!token) return false;
 
     const decodeToken = jwtDecode<JwtPayload>(token);
