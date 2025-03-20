afec53082bb9709602f376264cbc18a94689baab
andymedinadev <107598069+andymedinadev@users.noreply.github.com>
2025-03-20 12:35:39 -0300

    feat: enhance authentication service with registration and availability check

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -4,20 +4,34 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from '@environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-
   apiUrl = environment.API_URL;
 
-  constructor(
-    private http: HttpClient
-  ) { }
+  constructor(private http: HttpClient) {}
 
   login(email: string, password: string) {
     return this.http.post(`${this.apiUrl}/api/v1/auth/login`, {
       email,
-      password
+      password,
     });
+  }
+
+  register(name: string, email: string, password: string) {
+    return this.http.post(`${this.apiUrl}/api/v1/auth/register`, {
+      name,
+      email,
+      password,
+    });
+  }
+
+  isAvailable(email: string) {
+    return this.http.post<{ isAvailable: boolean }>(
+      `${this.apiUrl}/api/v1/auth/is-available`,
+      {
+        email,
+      },
+    );
   }
 }
