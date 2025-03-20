1b09c486b617222ed43a559838444c9f980d3aef
andymedinadev <107598069+andymedinadev@users.noreply.github.com>
2025-03-20 10:39:52 -0300

    feat: refactor login page and add path shortcuts

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { ButtonComponent } from '../../components/button/button.component';
+import { BackgroundComponent } from '../../components/background/background.component';
+import { HeaderComponent } from '../../components/header/header.component';
+import { LoginFormComponent } from '../../components/login-form/login-form.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ButtonComponent, RouterModule],
+  imports: [
+    BackgroundComponent,
+    HeaderComponent,
+    LoginFormComponent,
+    FooterComponent,
+    RouterModule,
+  ],
   templateUrl: './login.component.html',
 })
 export class LoginComponent {}
